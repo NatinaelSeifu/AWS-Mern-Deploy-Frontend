d03aa955feebcc66a938fddb06b123f14bec3c0d
Kirubel-Fikru <kirubelfikruwsh@gmail.com>
2023-06-26 11:57:19 +0300

    Slack test

--- src/Axios/axios.js
+++ src/Axios/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios"
 const instance = axios.create({
-    baseURL:"http://localhost:8000/api"
+  baseURL:
+    "http://Mern-Deployment-Demo-Backend-env.eba-bndx2ebk.us-east-1.elasticbeanstalk.com/api",
 })
 export default instance
