68547eb658fe4977851ca62594607b5c6f6831ba
Kirubel-Fikru <kirubelfikruwsh@gmail.com>
2023-07-04 12:02:05 +0300

    Connect

--- src/Axios/axios.js
+++ src/Axios/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 const instance = axios.create({
-  baseURL: "http://ec2-54-210-52-70.compute-1.amazonaws.com/api",
+  baseURL: "http://ec2-54-210-52-70.compute-1.amazonaws.com:8000/api",
 })
 export default instance
 //test  k
